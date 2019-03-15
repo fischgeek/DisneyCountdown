4d80d89842eb186807b0df9196029396a346c80e
Jeremiah Fischer <fischgeek@gmail.com>
2019-03-15 13:59:46 -0500

    quick

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,77 +1,103 @@
 $(document).ready(function () {
-	// if (!window.navigator.standalone == true) {
-	// 	$('#instructions').show();
-	// 	$('#innerContainer').hide();
-	// }
-	console.log(getURLParameter("date"))
-	
+	var tdate;
 	var counterPosition = 7
-	var imageBank = 42
+	var imageBank = 65
 	var seqNo = Math.floor(Math.random() * imageBank)
-	var excludes = [4, 6, 11, 12, 13, 20, 21, 23, 24, 27, 32, 35, 42]
-	seqNo = switchImage(seqNo, imageBank, "left")
-	// setInterval(function () { updateCounter() }, 2000);
-	updateCounter()
-	positionCounter(counterPosition)
+	var excludes = [43]
+	var installed = window.navigator.standalone
+	var isFirefox = typeof InstallTrigger !== 'undefined'
 
-	$('#countdown').on('touchend', function () {
-		counterPosition = positionCounter(counterPosition + 1)
+	if (!installed) {
+		$('#instructions-container').show()
+		$('#container').hide()
+		$('#date-picker-container').hide()
+	} else {
+		installed = true;
+		tdate = Cookies.get('disney-date')
+		tdate = new moment(tdate, "YYYY-MM-DD")
+		if (!tdate.isValid()) {
+			$('#container').hide()
+			$('#date-picker-container').show()
+			$('#date-picker').val(new moment().format('YYYY-MM-DD'))
+		} else {
+			$('#container').show()
+			$('#date-picker-container').hide()
+			seqNo = switchImage(seqNo, imageBank, "left")
+			updateCounter()
+			positionCounter(counterPosition)
+		}
+	}
+
+	$('#disney').on('touchend click', function () {
+		var ran = Math.floor(Math.random() * Math.floor(imageBank));
+		console.log(ran)
+		switchImage(ran, imageBank, "random")
+	})
+	
+	$('#done-button').on('touchend click', function () {
+		var x = $('#date-picker').val()
+		Cookies.set('disney-date', x, { expires: 365 })
+		tdate = new moment(x, "YYYY-MM-DD")
+		if (tdate.isValid()) {
+			$('#container').show()
+			$('#date-picker-container').hide()
+			counterPosition = positionCounter(counterPosition)
+			updateCounter()
+		}
+	})
+
+	var timer
+	$('#countdown').on('touchstart mousedown', function () {
+		timer = setTimeout(function () {
+			$('#container').hide()
+			$('#date-picker-container').show()
+			if (tdate.isValid()) {
+				$('#date-picker').val(tdate.format('YYYY-MM-DD'))
+			} else {
+				$('#date-picker').val(new moment().format('YYYY-MM-DD'))
+			}
+		}, 2000)
+	}).on('touchend mouseleave', function () {
+		clearTimeout(timer)
 	})
 
 	$('body').swipe({
 		swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-			if (direction == "left") {
-				seqNo++
-			} else if (direction == "right") {
-				seqNo--
-			} else if (direction == "up") {
-				counterPosition--
-			} else if (direction == "down") {
-				counterPosition++
+			if (installed) {
+				if (direction == "left") {
+					seqNo++
+				} else if (direction == "right") {
+					seqNo--
+				} else if (direction == "up") {
+					counterPosition--
+				} else if (direction == "down") {
+					counterPosition++
+				}
+				seqNo = switchImage(seqNo, imageBank, direction)
+				counterPosition = positionCounter(counterPosition)
 			}
-			console.log('swiped ' + direction)
-			seqNo = switchImage(seqNo, imageBank, direction)
-			counterPosition = positionCounter(counterPosition)
 		}
 	})
 
 	function updateCounter() {
-		console.log('updating counter...')
-		// set the date we're counting down to
-		var target_date = new Date("Dec 8, 2019").getTime();
-		// variables for time units
-		var days, hours, minutes, seconds;
-		// find the amount of "seconds" between now and target
-		var current_date = new Date().getTime();
-		var d = new Date().getDay();
-		var seconds_left = (target_date - current_date) / 1000;
-		days = parseInt(seconds_left / 86400);
-		seconds_left = seconds_left % 86400;
-		hours = parseInt(seconds_left / 3600);
-		seconds_left = seconds_left % 3600;
-		minutes = parseInt(seconds_left / 60);
-		seconds = parseInt(seconds_left % 60);
-		$("#countdown").text(days + 1);
+		var cDate = new moment()
+		var diff = tdate.diff(cDate, 'days')
+		$("#countdown").text(diff + 1);
 	}
 
 	function switchImage(imgNum, highest, dir) {
 		imgNum = (imgNum == imageBank ? 1 : imgNum)
 		imgNum = imgNum > highest ? 1 : imgNum
 		while (excludes.includes(imgNum)) {
-			console.log(imgNum + '. picking a new one...')
 			if (dir == "left") {
 				imgNum++
 			} else if (dir == "right") {
 				imgNum--
+			} else if (dir == "random") {
+				imgNum = 1
 			}
-			console.log('new imgNum: ' + imgNum)
 		}
-		$('body').css({
-			'background': 'url("images/' + imgNum + '.jpg")',
-			'background-repeat': 'no-repeat',
-			'background-size': 'cover',
-			'background-position': 'center'
-		});
+		$('#disney-image').attr('src', 'images/' + imgNum + '.jpg')
 		return imgNum
 	}
 
@@ -84,7 +110,6 @@ $(document).ready(function () {
 				break;
 			case 1:
 				$('#innerContainer').css({ 'top': 5, 'right': '', 'bottom': '', 'left': 5, 'text-align': 'center' })
-				console.log('value was 1')
 				break;
 			case 2:
 				$('#innerContainer').css({ 'top': 5, 'right': '', 'bottom': '', 'left': 5, 'text-align': 'right' })

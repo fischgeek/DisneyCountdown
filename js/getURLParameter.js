1341967a1382e3fdda2579db093bf7aa57bb3dd1
fischgeek <fischgeek@gmail.com>
2022-06-24 14:21:05 -0500

    removing ignored

--- js/getURLParameter.js
+++
@@ -1,8 +0,0 @@
-function getURLParameter(name) {
-	var x = (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [""])[1];
-	if (x) {
-		return decodeURIComponent(x.replace(/\+/g, '%20')) || undefined;
-	} else {
-		return undefined;
-	}
-}
